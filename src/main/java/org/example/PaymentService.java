b495ff0fe94c2cf07c4e6e3805c342932efc41a5
lenhatquang <quangln@grooo.vn>
2024-03-13 09:21:09 +0700

    Update PaymentService.java

--- src/main/java/org/example/PaymentService.java
+++ src/main/java/org/example/PaymentService.java
@@ -7,22 +7,17 @@ import java.util.stream.Collectors;
 
 public class PaymentService {
     private final AtomicInteger balance = new AtomicInteger();
-    private final Set<Bill> bills;
-    private final List<Transaction> transactions;
-    private final List<Schedule> schedules;
-
-    public PaymentService() {
-        transactions = new ArrayList<>();
-        bills = new HashSet<>();
-        schedules = new ArrayList<>();
-    }
+    private final Set<Bill> bills = new HashSet<>();
+    private final List<Transaction> transactions = new ArrayList<>();
+    private final List<Schedule> schedules = new ArrayList<>();
 
     public void addFunds(int amount) {
-        System.out.println("Your available balance: " + balance.addAndGet(amount));
+        balance.addAndGet(amount);
+        System.out.println("Your available balance: " + balance);
     }
 
     public void addBill(Bill bill) {
-        if(!bills.add(bill)) {
+        if (!bills.add(bill)) {
             System.out.println("Bill with id " + bill.getId() + " already exists.");
         }
     }
@@ -37,21 +32,13 @@ public class PaymentService {
                 .sorted(Comparator.comparing(Bill::getDueDate))
                 .collect(Collectors.toCollection(LinkedList::new));
 
-        ids.stream().filter(id -> selectedBills.stream().noneMatch(bill -> bill.getId() == id))
+        ids.stream()
+                .filter(id -> selectedBills.stream().noneMatch(bill -> bill.getId() == id))
                 .forEach(id -> System.out.println("Sorry! Not found a bill with id " + id));
 
         selectedBills.stream()
-            .filter(bill -> !bill.getState().equals("PAID"))
-            .forEach(bill -> {
-                if (bill.getAmount() <= balance.get()) {
-                    bill.setState("PAID");
-                    transactions.add(new Transaction(LocalDate.now(), bill));
-                    balance.addAndGet(-bill.getAmount());
-                    System.out.println("Payment has been completed for Bill with id " + bill.getId() + ". Your current balance is: " + balance);
-                } else {
-                    System.out.println("Sorry! Not enough fund to proceed with payment.");
-                }
-            });
+                .filter(bill -> !bill.getState().equals("PAID"))
+                .forEach(this::processPayment);
     }
 
     public void searchBillsByProvider(ProviderEnum provider) {
@@ -72,10 +59,10 @@ public class PaymentService {
             return;
         }
 
-        bills.stream()
-            .filter(bill -> bill.getState().equals("PAID"))
-            .findFirst()
-            .ifPresent(bill -> System.out.println("Sorry! Bill with id " + schedule.getBillId() + " has already been paid."));
+        if (bills.stream().anyMatch(bill -> bill.getId() == schedule.getBillId() && bill.getState().equals("PAID"))) {
+            System.out.println("Sorry! Bill with id " + schedule.getBillId() + " has already been paid.");
+            return;
+        }
 
         schedules.add(schedule);
         System.out.println("Bill with id " + schedule.getBillId() + " has been scheduled for payment on " + DateUtil.format(schedule.getScheduledPaymentDate()));
@@ -86,18 +73,17 @@ public class PaymentService {
     }
 
     private String billToString(Bill bill) {
-        StringBuilder builder = new StringBuilder();
-        builder.append(bill.getId())
-            .append(". ")
-            .append(bill.getType())
-            .append(" ")
-            .append(bill.getAmount())
-            .append(" ")
-            .append(bill.getDueDate())
-            .append(" ")
-            .append(bill.getState())
-            .append(" ")
-            .append(bill.getProvider());
-        return builder.toString();
+        return bill.getId() + ". " + bill.getType() + " " + bill.getAmount() + " " + bill.getDueDate() + " " + bill.getState() + " " + bill.getProvider();
+    }
+
+    private void processPayment(Bill bill) {
+        if (bill.getAmount() <= balance.get()) {
+            bill.setState("PAID");
+            transactions.add(new Transaction(LocalDate.now(), bill));
+            balance.addAndGet(-bill.getAmount());
+            System.out.println("Payment has been completed for Bill with id " + bill.getId() + ". Your current balance is: " + balance);
+        } else {
+            System.out.println("Sorry! Not enough fund to proceed with payment.");
+        }
     }
 }
